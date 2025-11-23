8ff2572cabb960bd83a7725456d87811700cc09e
Zharaskhan <m_jasik_2005@mail.ru>
2025-11-23 17:10:39 +0600

    build project

---
+++ src/main/java/com/example/slab9/Slab9Application.java
@@ -0,0 +1,13 @@
+package com.example.slab9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slab9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Slab9Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slab9/Slab9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slab9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Slab9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
